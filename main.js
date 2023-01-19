8434e7d7b0fb71adf1fe80215b254203ee37e337
Mateusz Szabatin <mateusz@szabatin.org>
2023-01-19 17:50:55 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log ("Wow widzę, że zaglądasz do konsoli w chromie brawo ty!");
+const name = 'Mateusz Szabatin';
+const age = 20;
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat`);
