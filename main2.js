37c27c1fc131e94a5f3269a03be3a84624dfb54e
Mateusz Szabatin <mateusz@szabatin.org>
2023-01-19 23:41:50 +0100

    feat: moved mandal, used width, height, display, padding, position, right, left, top bottom, margin

--- main2.js
+++
@@ -1,8 +0,0 @@
-console.log('Witaj na mojej stronie!');
-const name = 'Mateusz Szabatin';
-const age = 20;
-console.log(name);
-console.log(age);
-
-console.log (`Nazywam sie ${name} i mam ${age} lat`)
-console.log ("Wow widzę, że zaglądasz do konsoli w chromie brawo ty!")
